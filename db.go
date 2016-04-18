e1a41e16402943ad9e9336f5db8e6b93e1440607
Valera <mkoctuk@gmail.com>
2016-04-18 15:35:53 +0300

    new db

--- db.go
+++ db.go
@@ -4,111 +4,20 @@ import (
 	"fmt"
 	"log"
 	"strings"
-	"time"
 
-	"github.com/jinzhu/gorm"
+	"github.com/jmoiron/sqlx"
 	"github.com/serbe/kpp"
 	"github.com/serbe/ncp"
-	// pq need to gorm
+	// pq need to sqlx
 	_ "github.com/lib/pq"
+
+	sq "github.com/Masterminds/squirrel"
 )
-
-// Movie all values
-// ID            id
-// Section       Раздел форума
-// Name          Название
-// EngName       Английское название
-// Year          Год
-// Genre         Жанр
-// Country       Производство
-// Director      Режиссер
-// Producer      Продюсер
-// Actors        Актеры
-// Description   Описание
-// Age           Возраст
-// ReleaseDate   Дата мировой премьеры
-// RussianDate   Дата премьеры в России
-// Duration      Продолжительность
-// Kinopoisk     Рейтинг кинопоиска
-// Imdb          Рейтинг IMDb
-// Poster        Имя файла постера
-// PosterURL     Сетевая ссылка на постер
-// UpdatedAt     Дата обновления записи БД
-// CreatedAt     Дата создания записи БД
-type Movie struct {
-	ID          int64     `gorm:"column:id"             db:"id"             sql:"AUTO_INCREMENT"`
-	Section     string    `gorm:"column:section"        db:"section"        sql:"type:text"`
-	Name        string    `gorm:"column:name"           db:"name"           sql:"type:text"`
-	EngName     string    `gorm:"column:eng_name"       db:"eng_name"       sql:"type:text"`
-	Year        int64     `gorm:"column:year"           db:"year"`
-	Genre       []string  `gorm:"column:genre"          db:"genre"          sql:"type:text[]"`
-	Country     []string  `gorm:"column:country"        db:"country"        sql:"type:text[]"`
-	RawCountry  string    `gorm:"column:raw_country"    db:"raw_country"    sql:"type:text"`
-	Director    []string  `gorm:"column:director"       db:"director"       sql:"type:text[]"`
-	Producer    []string  `gorm:"column:producer"       db:"producer"       sql:"type:text[]"`
-	Actors      []string  `gorm:"column:actors"         db:"actors"         sql:"type:text[]"`
-	Description string    `gorm:"column:description"    db:"description"    sql:"type:text"`
-	Age         string    `gorm:"column:age"            db:"age"            sql:"type:text"`
-	ReleaseDate string    `gorm:"column:release_date"   db:"release_date"   sql:"type:text"`
-	RussianDate string    `gorm:"column:russian_date"   db:"russian_date"   sql:"type:text"`
-	Duration    string    `gorm:"column:duration"       db:"duration"       sql:"type:text"`
-	Kinopoisk   float64   `gorm:"column:kinopoisk"      db:"kinopoisk"`
-	IMDb        float64   `gorm:"column:imdb"           db:"imdb"`
-	Poster      string    `gorm:"column:poster"         db:"poster"         sql:"type:text"`
-	PosterURL   string    `gorm:"column:poster_url"     db:"poster_url"     sql:"type:text"`
-	UpdatedAt   time.Time `gorm:"column:updated_at"     db:"updated_at"`
-	CreatedAt   time.Time `gorm:"column:created_at"     db:"created_at"`
-}
-
-// Torrent all values
-// ID            id
-// FilmID        Указатель на фильм
-// DateCreate    Дата создания раздачи
-// Href          Ссылка
-// Torrent       Ссылка на torrent
-// Magnet        Ссылка на magnet
-// NNM           Рейтинг nnm-club
-// SubtitlesType Вид субтитров
-// Subtitles     Субтитры
-// Video         Видео
-// Quality       Качество видео
-// Resolution    Разрешение видео
-// Audio1        Аудио1
-// Audio2        Аудио2
-// Audio3        Аудио3
-// Translation   Перевод
-// Size          Размер
-// Seeders       Количество раздающих
-// Leechers      Количество скачивающих
-// UpdatedAt     Дата обновления записи БД
-// CreatedAt     Дата создания записи БД
-type Torrent struct {
-	ID            int64     `gorm:"column:id"             db:"id"             sql:"AUTO_INCREMENT"`
-	MovieID       int64     `gorm:"column:movie_id"       db:"movie_id"`
-	DateCreate    string    `gorm:"column:date_create"    db:"date_create"    sql:"type:text"`
-	Href          string    `gorm:"column:href"           db:"href"           sql:"type:text"`
-	Torrent       string    `gorm:"column:torrent"        db:"torrent"        sql:"type:text"`
-	Magnet        string    `gorm:"column:magnet"         db:"magnet"         sql:"type:text"`
-	NNM           float64   `gorm:"column:nnm"            db:"nnm"`
-	SubtitlesType string    `gorm:"column:subtitles_type" db:"subtitles_type" sql:"type:text"`
-	Subtitles     string    `gorm:"column:subtitles"      db:"subtitles"      sql:"type:text"`
-	Video         string    `gorm:"column:video"          db:"video"          sql:"type:text"`
-	Quality       string    `gorm:"column:quality"        db:"quality"        sql:"type:text"`
-	Resolution    string    `gorm:"column:resolution"     db:"resolution"     sql:"type:text"`
-	Audio1        string    `gorm:"column:audio1"         db:"audio1"         sql:"type:text"`
-	Audio2        string    `gorm:"column:audio2"         db:"audio2"         sql:"type:text"`
-	Audio3        string    `gorm:"column:audio3"         db:"audio3"         sql:"type:text"`
-	Translation   string    `gorm:"column:translation"    db:"translation"    sql:"type:text"`
-	Size          int64     `gorm:"column:size"           db:"size"`
-	Seeders       int64     `gorm:"column:seeders"        db:"seeders"`
-	Leechers      int64     `gorm:"column:leechers"       db:"leechers"`
-	UpdatedAt     time.Time `gorm:"column:updated_at"     db:"updated_at"`
-	CreatedAt     time.Time `gorm:"column:created_at"     db:"created_at"`
-}
 
 // App struct variables
 type App struct {
-	db    *gorm.DB
+	db    *sqlx.DB
+	psql  sq.StatementBuilderType
 	net   *ncp.NCp
 	hd    string
 	px    string
@@ -124,24 +33,24 @@ func appInit() (*App, error) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		dbConnect, err := gorm.Open("postgres", fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s", conf.Pq.User, conf.Pq.Password, conf.Pq.Dbname, conf.Pq.Sslmode))
+		dbConnect, err := sqlx.Open("postgres", fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s", conf.Pq.User, conf.Pq.Password, conf.Pq.Dbname, conf.Pq.Sslmode))
 		if err != nil {
 			log.Fatal("db open ", err)
 			return app, err
 		}
-		dbConnect.DB()
-		dbConnect.DB().Ping()
-		dbConnect.DB().SetMaxIdleConns(10)
-		dbConnect.DB().SetMaxOpenConns(100)
-		dbConnect.AutoMigrate(&Movie{})
-		dbConnect.AutoMigrate(&Torrent{})
-		dbConnect.LogMode(conf.DebugDB)
+		err = dbConnect.Ping()
+		if err != nil {
+			log.Fatal("db ping ", err)
+			return app, err
+		}
 		inetConnect, err := ncp.Init(conf.Nnm.Login, conf.Nnm.Password, conf.Address, conf.Px)
 		if err != nil {
 			log.Println("net init ", err)
 			return app, err
 		}
 		_ = createDir(conf.Hd)
+		psql := sq.StatementBuilder.PlaceholderFormat(sq.Dollar)
+		app.psql = psql
 		app.db = dbConnect
 		app.net = inetConnect
 		app.hd = conf.Hd
@@ -156,14 +65,10 @@ func (a *App) createMovie(ncf ncp.Film) (int64, error) {
 		movie Movie
 		kp    kpp.KP
 	)
+	movie.Section = ncf.Section
 	movie.Name = ncf.Name
 	movie.EngName = ncf.EngName
 	movie.Year = ncf.Year
-	movie.Genre = ncf.Genre
-	movie.Country = ncf.Country
-	movie.Director = ncf.Director
-	movie.Producer = ncf.Producer
-	movie.Actors = ncf.Actors
 	movie.Description = ncf.Description
 	movie.Age = ncf.Age
 	movie.ReleaseDate = ncf.ReleaseDate
@@ -176,7 +81,28 @@ func (a *App) createMovie(ncf ncp.Film) (int64, error) {
 	}
 	movie.PosterURL = ncf.Poster
 	movie.Poster, _ = a.getPoster(movie.PosterURL)
+	sql := app.psql.Insert("movies").Columns("section", "name",
+		eng_name text,
+		year int8,
+		genre text,
+		raw_country text,
+		description text,
+		age text,
+		release_date text,
+		russian_date text,
+		duration text,
+		kinopoisk float8,
+		imdb float8,
+		poster text,
+		poster_url text)
 	err = a.db.Model(Movie{}).Create(&movie).Error
+	genres := ncf.Genre
+	countries := ncf.Country
+	directors := ncf.Director
+	producer := ncf.Producer
+	actors := ncf.Actor
+
+	fmt.Println(err)
 	return movie.ID, err
 }
 
@@ -210,7 +136,9 @@ func (a *App) createTorrent(ncf ncp.Film) error {
 	tor.Size = ncf.Size
 	tor.Seeders = ncf.Seeders
 	tor.Leechers = ncf.Leechers
-	return a.db.Model(Torrent{}).Create(&tor).Error
+	err = a.db.Model(Torrent{}).Create(&tor).Error
+	fmt.Println(err)
+	return err
 }
 
 func (a *App) getMovies() ([]Movie, error) {
